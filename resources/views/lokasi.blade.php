f14a3bb9c823c959aceb197f131fc7cb599c38ad
dwihisyam <dwihisyam3@gmail.com>
2019-08-12 16:30:54 +0700

    masukkin map

--- resources/views/lokasi.blade.php
+++ resources/views/lokasi.blade.php
@@ -7,7 +7,9 @@
 @section('container')
 <section>
     <div class="container p_50">
-        <div id="googleMap" style="width:100%;height:70%;"></div>
+        <div class="map-responsive" style="width:100%;height:70%;">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.6664669606857!2d106.82496411434013!3d-6.1753870622318665!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f5d2e764b12d%3A0x3d2ad6e1e0e9bcc8!2sMonumen+Nasional!5e0!3m2!1sid!2sid!4v1565601302332!5m2!1sid!2sid" width="100%" height="100%" frameborder="0" style="border:0" allowfullscreen></iframe></iframe>
+        </div>
     </div>
 </section>
 @endsection
